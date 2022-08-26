226fc5045d4beb9d483ce36f66ae228210323391
nam tran <tranxuannam16021997@gmail.com>
2022-08-26 14:16:50 +0700

    second commit

---
+++ Assets/Scripts/Consumable/Types/Invincibility.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Invincibility : Consumable
+{
+    public override string GetConsumableName()
+    {
+        return "Map Invincible";
+    }
+
+    public override ConsumableType GetConsumableType()
+    {
+        return ConsumableType.MAP_INVINCIBILITY;
+    }
+
+    public override int GetPrice()
+    {
+        return 1500;
+    }
+
+	public override int GetPremiumCost()
+	{
+		return 5;
+	}
+
+	public override void Tick(CharacterInputController c)
+    {
+        base.Tick(c);
+
+        c.characterCollider.SetInvincibleExplicit(true);
+    }
+
+    public override IEnumerator Started(CharacterInputController c)
+    {
+        yield return base.Started(c);
+        c.characterCollider.SetInvincible(duration);
+    }
+
+    public override void Ended(CharacterInputController c)
+    {
+        base.Ended(c);
+        c.characterCollider.SetInvincibleExplicit(false);
+    }
+}
